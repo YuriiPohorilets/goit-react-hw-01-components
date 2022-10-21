e392fd5252ea07cab2f0fad079b0df4761ea314e
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-21 23:07:31 +0300

    add component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import user from 'data/user.json';
+import { Profile } from 'components/Profile/Profile';
+
 export const App = () => {
-  
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
